ef5ecad89468883950dced2ec027c0db1f341eee
Edward Ogbalor <thunderogbalor@gmail.com>
2025-04-30 15:40:25 -0700

    Fix: expose Flask-SocketIO app on correct host and port

--- app.py
+++ app.py
@@ -40,5 +40,6 @@ def handle_disconnect():
             break
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5001, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=10000, debug=True, allow_unsafe_werkzeug=True)
+
 
