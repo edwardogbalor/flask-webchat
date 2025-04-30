b9365399d4bf79f6bbe62cfcf707a34486ae3710
Edward Ogbalor <thunderogbalor@gmail.com>
2025-04-30 15:42:39 -0700

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -40,6 +42,6 @@ def handle_disconnect():
             break
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=True, allow_unsafe_werkzeug=True)
 
 
